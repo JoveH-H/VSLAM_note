34cf6a8229fa6c00c6b7aedb2aa2e63e8707518a
氢键 <42627185+JoveH-H@users.noreply.github.com>
2019-09-26 16:39:54 +0800

    Add files via upload

---
+++ 018/useSophus_SE3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include "sophus/se3.h"
+
+int main( int argc, char** argv )
+{
+    // 沿Z轴转90度的旋转矩阵
+    Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
+    Eigen::Quaterniond q(R);            // 四元数
+    Eigen::Vector3d t(1,0,0);           // 沿X轴平移1
+    Sophus::SE3 SE3_Rt(R, t);           // 从R,t构造SE(3)
+    Sophus::SE3 SE3_qt(q,t);            // 从q,t构造SE(3)
+    cout<<"SE3 from R,t= "<<endl<<SE3_Rt<<endl;
+    cout<<"SE3 from q,t= "<<endl<<SE3_qt<<endl;
+
+    // 李代数se(3) 是一个六维向量，方便起见先typedef一下
+    typedef Eigen::Matrix<double,6,1> Vector6d;
+    Vector6d se3 = SE3_Rt.log();
+    cout<<"se3 = "<<se3.transpose()<<endl;
+
+    // 观察输出，会发现在Sophus中，se(3)的平移在前，旋转在后.
+    // hat 为向量到反对称矩阵
+    cout<<"se3 hat = "<<endl<<Sophus::SE3::hat(se3)<<endl;
+
+    // 相对的，vee为反对称矩阵到向量
+    cout<<"se3 hat vee = "<<Sophus::SE3::vee( Sophus::SE3::hat(se3) ).transpose()<<endl;
+    
+    // 最后，演示一下更新
+    Vector6d update_se3; //更新量
+    update_se3.setZero();
+    update_se3(0,0) = 1e-4d;
+    Sophus::SE3 SE3_updated = Sophus::SE3::exp(update_se3)*SE3_Rt;
+    cout<<"SE3 updated = "<<endl<<SE3_updated.matrix()<<endl;
+    
+    return 0;
+}
