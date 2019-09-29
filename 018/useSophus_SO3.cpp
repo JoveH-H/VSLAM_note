b7e7f7aaaf7d7329dc79cfc203287c484a189d49
氢键 <42627185+JoveH-H@users.noreply.github.com>
2019-09-29 14:00:23 +0800

    Add files via upload

--- 018/useSophus_SO3.cpp
+++ 018/useSophus_SO3.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 #include <cmath>
-using namespace std; 
+using namespace std;
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
 #include "sophus/so3.h"
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
     // 沿Z轴转90度的旋转矩阵
-    Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
-    
+    Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
+
     Sophus::SO3 SO3_R(R);               // Sophus::SO(3)可以直接从旋转矩阵构造
-    Sophus::SO3 SO3_v( 0, 0, M_PI/2 );  // 亦可从旋转向量构造
+    Sophus::SO3 SO3_v(0, 0, M_PI / 2);  // 亦可从旋转向量构造
     Eigen::Quaterniond q(R);            // 或者四元数
-    Sophus::SO3 SO3_q( q );
+    Sophus::SO3 SO3_q(q);
     // 上述表达方式都是等价的
 
     // 输出SO(3)时，以so(3)形式输出
-    cout<<"SO(3) from matrix: "<<SO3_R<<endl;
-    cout<<"SO(3) from vector: "<<SO3_v<<endl;
-    cout<<"SO(3) from quaternion :"<<SO3_q<<endl;
-    
+    cout << "SO(3) from matrix: " << SO3_R << endl;
+    cout << "SO(3) from vector: " << SO3_v << endl;
+    cout << "SO(3) from quaternion :" << SO3_q << endl;
+
     // 使用对数映射获得它的李代数
     Eigen::Vector3d so3 = SO3_R.log();
     // transpose纯粹是为了输出美观一些
-    cout<<"so3 = "<<so3.transpose()<<endl;
+    cout << "so3 = " << so3.transpose() << endl;
 
     // hat 为向量到反对称矩阵
-    cout<<"so3 hat=\n"<<Sophus::SO3::hat(so3)<<endl;
+    cout << "so3 hat=\n" << Sophus::SO3::hat(so3) << endl;
 
     // 相对的，vee为反对称矩阵到向量
-    cout<<"so3 hat vee= "<<Sophus::SO3::vee( Sophus::SO3::hat(so3) ).transpose()<<endl; 
-    
+    cout << "so3 hat vee= " << Sophus::SO3::vee(Sophus::SO3::hat(so3)).transpose() << endl;
+
     // 增量扰动模型的更新
     Eigen::Vector3d update_so3(1e-4, 0, 0); //假设更新量为这么多
-    Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3)*SO3_R;
-    cout<<"SO3 updated = "<<SO3_updated<<endl;
-    
+    Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3) * SO3_R;
+    cout << "SO3 updated = " << SO3_updated << endl;
+
     return 0;
 }
